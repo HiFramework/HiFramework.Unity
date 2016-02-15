0710b0c20acd8d25f838329e846ff8c45f767e64
Hiram <hiramtan@qq.com>
2016-02-15 10:07:52 +0800

    删除测试代码,只保留framework

--- HiFramework/Assets/Script/Logic/CommonDefine.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class CommonDefine
-{
-    public static float transitionDuration = 0.2f;//淡出时间
-}
--- HiFramework/Assets/Test/TestLogic/ActorDataTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ActorDataTest : ActorData
-{
-    public ActorDataTest(Actor paramActor) : base(paramActor)
-    {
-    }
-}
