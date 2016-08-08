db9ef3b3f11de7a1e2d32795b9a45b82a1966e8a
Hiram <hiramtan@live.com>
2016-08-08 16:53:51 +0800

    添加自定义float

---
+++ HiFramework/Assets/Script/Core/HiFloat.cs
@@ -0,0 +1,57 @@
+﻿//*********************************************************************
+// Description:
+// Author: hiramtan@live.com
+//*********************************************************************
+using System;
+
+public struct HiFloat
+{
+    /// <summary>
+    /// 保留小数点后6位,第7位四色五入
+    /// </summary>
+    private const int length = 6;
+    private float value;
+    private HiFloat(float param)
+    {
+        value = (float)Math.Round(param, length);
+    }
+    public static implicit operator HiFloat(float param)
+    {
+        return new HiFloat(param);
+    }
+
+    public static implicit operator float(HiFloat param)
+    {
+        return param.value;
+    }
+
+    public static HiFloat operator ++(HiFloat param)
+    {
+        return ++param.value;
+    }
+
+    public static HiFloat operator --(HiFloat param)
+    {
+        return --param.value;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (!(obj is HiFloat))
+        {
+            throw new Exception("类型不一致");
+        }
+        HiFloat temp = (HiFloat)obj;
+        if (temp.value == value)
+            return true;
+        return false;
+    }
+    public override string ToString()
+    {
+        return value.ToString();
+    }
+    public override int GetHashCode()
+    {
+        return value.GetHashCode();
+    }
+}
