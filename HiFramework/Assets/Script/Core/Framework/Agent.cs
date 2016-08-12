20c9a14436e8842c70cfa530243bf2d382230bcb
Hiram <hiramtan@live.com>
2016-08-12 11:48:35 +0800

    优化tick添加逻辑

---
+++ HiFramework/Assets/Script/Core/Framework/Agent.cs
@@ -0,0 +1,62 @@
+﻿//****************************************************************************
+// Description:
+// Author: hiramtan@qq.com
+//****************************************************************************
+using UnityEngine;
+using System.Collections;
+using System;
+
+namespace HiFramework
+{
+    public abstract class Agent : Manager, ILogic
+    {
+        private bool disposed;
+        public void Dispatch(object paramKey, Message paramMessage)
+        {
+            Facade.Mediator.Dispatch(paramKey, paramMessage);
+        }
+
+        public void Register<T>(object paramKey) where T : ILogic
+        {
+            Facade.Mediator.Register<T>(paramKey);
+        }
+
+        public abstract void OnTick();
+
+        public abstract void OnMessage(Message paramMessage);
+
+        public void Unregister(object paramKey)
+        {
+            Facade.Mediator.Unregister(paramKey);
+        }
+        public void AddToTickList()
+        {
+            Facade.GameTick.AddToTickList(this);
+        }
+
+        public void RemoveFromTickList()
+        {
+            Facade.GameTick.RemoveFromTickList(this);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        ~Agent()
+        {
+            Dispose(false);
+        }
+        protected virtual void Dispose(bool paramDisposing)
+        {
+            if (disposed)
+                return;
+            if (paramDisposing)
+            {
+
+            }
+            disposed = true;
+        }
+    }
+}
---
+++ HiFramework/Assets/Script/Core/Framework/Interface/IMessageReceive.cs
@@ -0,0 +1,14 @@
+﻿//*********************************************************************
+// Description:
+// Author: hiramtan@live.com
+//*********************************************************************
+using UnityEngine;
+using System.Collections;
+
+namespace HiFramework
+{
+    public interface IMessageReceive
+    {
+        void OnMessage(Message paramMessage);
+    }
+}
