836d68f5f82cb78cf6402188419c018d5debd13f
Hiram <hriamtan@qq.com>
2017-08-08 12:07:10 +0800

    添加www任务

---
+++ HiFramework/Assets/Script/Core/AsyncTask/AsyncWWWTask.cs
@@ -0,0 +1,36 @@
+﻿using HiFramework;
+using UnityEngine;
+
+public class AsyncWWWTask : AsyncTask
+{
+    private WWW www;
+    public AsyncWWWTask(string url)
+    {
+        www = new WWW(url);
+    }
+
+    protected override void Update()
+    {
+        Debug.Log(www.progress);
+
+        if (www.isDone)
+            isDone = true;
+    }
+
+    protected override void _Complate()
+    {
+        action(www);
+    }
+}
+
+//public class TestAsyncWWWTask : MonoBehaviour
+//{
+//    void Start()
+//    {
+//        string url = "www.g.com";
+//        new AsyncWWWTask(url).Start().Finish((p) =>
+//        {
+//            Debug.Log("download finish:" + ((WWW)p).bytes);
+//        });
+//    }
+//}
