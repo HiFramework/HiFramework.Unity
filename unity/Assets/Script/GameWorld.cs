dc2884c9bb84751df9b2eacf089182d1e7944921
Hiram <hriamtan@qq.com>
2017-08-22 11:23:23 +0800

    添加gameworld

---
+++ unity/Assets/Script/GameWorld.cs
@@ -0,0 +1,33 @@
+﻿//****************************************************************************
+// Description:
+// Author: hiramtan@live.com
+//***************************************************************************
+
+using System.Collections;
+using System.Collections.Generic;
+using HiFramework;
+using UnityEngine;
+
+public class GameWorld : MonoBehaviour
+{
+    private static bool isExist;
+    // Use this for initialization
+    void Start()
+    {
+        if (!isExist)
+        {
+            DontDestroyOnLoad(gameObject);
+            isExist = true;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Facade.GameTick.OnTick();
+    }
+}
