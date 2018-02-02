3f22a6398eac260bf5b5e7d3cd68829c93dc2786
Hiram <hriamtan@qq.com>
2018-02-02 14:45:21 +0800

    add zip and socket component

---
+++ HiFramework/HiFramework/Component/Socket/SocketComponent.cs
@@ -0,0 +1,19 @@
+﻿/****************************************************************
+ * Description: 
+ *  haven't finish
+ * Author: hiramtan@live.com
+ *////////////////////////////////////////////////////////////////////////
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HiFramework
+{
+    class SocketComponent
+    {
+    }
+}
