a8e99aa88166996e950aaabda5d9bf400103e826
Hiram <hiramtan@live.com>
2018-07-02 11:27:44 +0800

    add unit test

---
+++ HiFramework/HiFrameworkTests/Core/CenterTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HiFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiFramework.Tests
+{
+    [TestClass()]
+    public class CenterTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            var c = Center.Get<TickComponent>();
+            Assert.IsNotNull(c);
+        }
+
+        [TestMethod()]
+        public void IsExistTest()
+        {
+            var c = Center.Get<TickComponent>();
+            var isTrue = Center.IsExist<TickComponent>();
+            Assert.IsTrue(isTrue);
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            var c = Center.Get<TickComponent>();
+            Center.Remove<TickComponent>();
+        }
+    }
+}
