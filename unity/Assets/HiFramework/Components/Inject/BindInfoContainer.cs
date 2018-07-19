b04e61ded878d6348ba1afdaab8ecaec49e6e2ab
Hiram <hiramtan@live.com>
2018-07-19 20:07:19 +0800

    add inject logic

--- unity/Assets/HiFramework/Components/Inject/BindInfoContainer.cs
+++ unity/Assets/HiFramework/Components/Inject/BindInfoContainer.cs
@@ -5,6 +5,9 @@
  * Author: hiramtan@live.com
 ***************************************************************/
 
+using System;
+using System.Collections.Generic;
+
 namespace HiFramework
 {
     /// <summary>
@@ -13,12 +16,28 @@ namespace HiFramework
     public class BindInfoContainer
     {
         /// <summary>
+        /// Bindinfos 
+        /// </summary>
+        private List<BindInfo> bindInfos = new List<BindInfo>();
+
+        /// <summary>
         /// 添加绑定信息
         /// </summary>
         /// <param name="bindInfo"></param>
         public void Add(BindInfo bindInfo)
         {
+            bindInfos.Add(bindInfo);
+        }
 
+        /// <summary>
+        /// Get object with type and AsName
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="asName"></param>
+        /// <returns></returns>
+        public object GetObject(Type type, string asName)
+        {
+            return bindInfos.Find((x) => { return (x.Type == type && x.AsName == asName); });
         }
     }
 }
--- unity/Assets/HiFramework/Components/Inject/InjectComponent.cs
+++ unity/Assets/HiFramework/Components/Inject/InjectComponent.cs
@@ -6,12 +6,13 @@
 ***************************************************************/
 
 using System;
+using System.Reflection;
 
 namespace HiFramework
 {
     public class InjectComponent : IInject
     {
-        BindInfoContainer bindInfoContainer = new BindInfoContainer();
+        private readonly BindInfoContainer bindInfoContainer = new BindInfoContainer();
         /// <summary>
         /// Bind a type to a instance
         /// </summary>
@@ -39,7 +40,64 @@ namespace HiFramework
         /// <param name="args"></param>
         public void Inject(object args)
         {
-            throw new System.NotImplementedException();
+            InjectField(args);
+            InjectProperty(args);
+        }
+
+        /// <summary>
+        /// Inject field
+        /// </summary>
+        /// <param name="args"></param>
+        private void InjectField(object obj)
+        {
+            var type = obj.GetType();
+            var fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
+            for (int i = 0; i < fields.Length; i++)
+            {
+                var attributes = fields[i].GetCustomAttributes(typeof(InjectAttribute), true);
+                if (attributes.Length == 0)//have no inject attribute
+                    continue;
+                InjectAttribute injectAttribute = null;
+                for (int j = 0; j < attributes.Length; j++)
+                {
+                    if (attributes[j] is InjectAttribute)
+                    {
+                        injectAttribute = attributes[j] as InjectAttribute;
+                        break;
+                    }
+                }
+                var toObj = bindInfoContainer.GetObject(fields[i].FieldType, injectAttribute.AsName);
+                AssertThat.IsNotNull(toObj, "Bind to object is null");
+                fields[i].SetValue(obj, toObj);
+            }
+        }
+
+        /// <summary>
+        /// Inject property
+        /// </summary>
+        /// <param name="args"></param>
+        private void InjectProperty(object obj)
+        {
+            var type = obj.GetType();
+            var propertys = type.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
+            for (int i = 0; i < propertys.Length; i++)
+            {
+                var attributes = propertys[i].GetCustomAttributes(typeof(InjectAttribute), true);
+                if (attributes.Length == 0)//have no inject attribute
+                    continue;
+                InjectAttribute injectAttribute = null;
+                for (int j = 0; j < attributes.Length; j++)
+                {
+                    if (attributes[j] is InjectAttribute)
+                    {
+                        injectAttribute = attributes[j] as InjectAttribute;
+                        break;
+                    }
+                }
+                var toObj = bindInfoContainer.GetObject(propertys[i].PropertyType, injectAttribute.AsName);
+                AssertThat.IsNotNull(toObj, "Bind to object is null");
+                propertys[i].SetValue(obj, toObj, null);
+            }
         }
     }
 }
