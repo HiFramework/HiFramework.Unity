c2c8b572e582a8f1dc8a3135e9d3963c15c0b79d
Hiram <hiramtan@live.com>
2018-07-31 20:42:25 +0800

    修改逻辑

--- unity/Assets/HiFramework/Components/String/StringComponent.cs
+++ unity/Assets/HiFramework/Components/String/StringComponent.cs
@@ -5,11 +5,17 @@
  ****************************************************************************/
 
 using System;
+using System.Text;
 
 namespace HiFramework
 {
     public class StringComponent : Component
     {
+        /// <summary>
+        /// 格式化字符串
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
         public static string FormatInfo(string info)
         {
             if (info == null)
@@ -17,6 +23,21 @@ namespace HiFramework
                 throw new ArgumentNullException("info is null");
             }
             return string.Format("[{0}]", info);
+        }
+
+        /// <summary>
+        /// 拼接字符串
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static string Get(params string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < args.Length; i++)
+            {
+                sb.Append(args[i]);
+            }
+            return sb.ToString();
         }
     }
 }
