3e1ee166d265931d043e58dbf8545cbe59c48853
Hiram <hiramtan@live.com>
2019-03-27 11:19:33 +0800

    add example file

---
+++ unity/Assets/Example1/Example1.cs
@@ -0,0 +1,18 @@
+﻿using HiFramework;
+using UnityEngine;
+
+public class Example1 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Center.Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Center.Tick(Time.deltaTime);
+    }
+}
