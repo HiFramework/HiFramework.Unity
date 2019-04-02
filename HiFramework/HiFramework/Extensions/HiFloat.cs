76e6a68b257aa90c36f3e9cd5ea258269b7beafc
Hiram <hiramtan@live.com>
2019-04-02 17:19:51 +0800

    add new float type

---
+++ HiFramework/HiFramework/Extensions/HiFloat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HiFramework
+{
+    public struct HiFloat
+    {
+        private const int Length = 7;
+        private float _value;
+
+        public float Value
+        {
+            get { return _value; }
+        }
+
+        private HiFloat(float value)
+        {
+            _value = (float) Math.Round(value, Length);
+        }
+
+        public static implicit operator HiFloat(float value)
+        {
+            return new HiFloat(value);
+        }
+
+        public static implicit operator float(HiFloat value)
+        {
+            return value._value;
+        }
+
+        public static HiFloat operator ++(HiFloat value)
+        {
+            return ++value._value;
+        }
+
+        public static HiFloat operator --(HiFloat value)
+        {
+            return --value._value;
+        }
+    }
+}
