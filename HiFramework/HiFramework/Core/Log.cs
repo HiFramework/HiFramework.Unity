4fc56d3072eb7eeca021154aa3a8a392e1d7f48e
Hiram <hiramtan@live.com>
2019-06-29 17:55:36 +0800

    add log proxy

--- HiFramework/HiFramework/Core/Log.cs
+++ HiFramework/HiFramework/Core/Log.cs
@@ -1,55 +1,51 @@
-﻿/****************************************************************************
- * Description:Fast dictionary logic and not use lingQ
- *
- * Author: hiramtan@live.com
- ****************************************************************************/
+﻿///****************************************************************************
+// * Description:Common log processer
+// *
+// * Author: hiramtan@live.com
+// ****************************************************************************/
 
-//#define UNITY
+//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Text;
 
-#define VISUAL
+//namespace HiFramework
+//{
+//    public static class Log
+//    {
+//        public static bool IsDebugOn;
+//        public static bool IsWanningOn;
+//        public static bool IsErrorOn;
 
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
+//        public static void Print(params object[] obj)
+//        {
+//#if VISUAL
+//            Console.WriteLine(obj);
+//#else
 
-namespace HiFramework
-{
-    public static class Log
-    {
-        public static bool IsDebugOn;
-        public static bool IsWanningOn;
-        public static bool IsErrorOn;
+//#endif
+//        }
 
-        public static void Print(params object[] obj)
-        {
-#if VISUAL
-            Console.WriteLine(obj);
-#else
+//        public static void Wanning(params object[] obj)
+//        {
+//#if VISUAL
+//            Console.WriteLine(obj);
+//#else
 
-#endif
-        }
+//#endif
+//        }
 
-        public static void Wanning(params object[] obj)
-        {
-#if VISUAL
-            Console.WriteLine(obj);
-#else
+//        public static void Error(params object[] obj)
+//        {
+//#if VISUAL
+//            for (int i = 0; i < obj.Length; i++)
+//            {
+//                throw new Exception(obj[i].ToString());
+//            }
+//#else
 
-#endif
-        }
-
-        public static void Error(params object[] obj)
-        {
-#if VISUAL
-            for (int i = 0; i < obj.Length; i++)
-            {
-                throw new Exception(obj[i].ToString());
-            }
-#else
-
-#endif
-        }
-    }
-}
+//#endif
+//        }
+//    }
+//}
---
+++ HiFramework/HiFramework/Core/Log/Log.cs
@@ -0,0 +1,43 @@
+﻿///****************************************************************************
+// * Description:Common log processer
+// *
+// * Author: hiramtan@live.com
+// ****************************************************************************/
+
+
+namespace HiFramework
+{
+    public static class Log
+    {
+        private static ILogProxy _logProxy;
+
+        public static void SetLogProxy(ILogProxy logProxy)
+        {
+            _logProxy = logProxy;
+        }
+
+        public static void Print(params object[] args)
+        {
+            if (_logProxy != null)
+            {
+                _logProxy.Print(args);
+            }
+        }
+
+        public static void Warnning(params object[] args)
+        {
+            if (_logProxy != null)
+            {
+                _logProxy.Warnning(args);
+            }
+        }
+
+        public static void Error(params object[] args)
+        {
+            if (_logProxy != null)
+            {
+                _logProxy.Error(args);
+            }
+        }
+    }
+}
